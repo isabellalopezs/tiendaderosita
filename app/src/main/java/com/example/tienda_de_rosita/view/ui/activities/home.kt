2dad5cabcdee635dab7495e66d66e961bc3f94ed
NathaN <jhonatandlopez@gmail.com>
2022-10-29 21:41:12 -0500

    Actualizacion proyecto

--- app/src/main/java/com/example/tienda_de_rosita/view/ui/activities/home.kt
+++ app/src/main/java/com/example/tienda_de_rosita/view/ui/activities/home.kt
@@ -1,12 +1,17 @@
 package com.example.tienda_de_rosita.view.ui.activities
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import com.example.tienda_de_rosita.R
 
 class home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
     }
 }
