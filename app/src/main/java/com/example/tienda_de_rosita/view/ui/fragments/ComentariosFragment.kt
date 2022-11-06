d5d0e64a8ce5dceeab47ccf05e1e11d18de869e9
NathaN <jhonatandlopez@gmail.com>
2022-11-06 10:23:44 -0500

    Actualizacion e implementacion de los fragments

--- app/src/main/java/com/example/tienda_de_rosita/view/ui/fragments/ComentariosFragment.kt
+++ app/src/main/java/com/example/tienda_de_rosita/view/ui/fragments/ComentariosFragment.kt
@@ -1,60 +1,43 @@
 package com.example.tienda_de_rosita.view.ui.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.tienda_de_rosita.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ComentariosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ComentariosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comentarios, container, false)
+        val view=inflater.inflate(R.layout.fragment_comentarios, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ComentariosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ComentariosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btm = view.findViewById<BottomNavigationView>(R.id.buttonNavigationMenu)
+        btm.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.configuracionm -> findNavController().navigate(R.id.action_comentariosFragment2_to_configuracionFragment)
+                R.id.homem -> findNavController().navigate(R.id.action_comentariosFragment2_to_menuFragment)
+                R.id.carritom -> findNavController().navigate(R.id.action_comentariosFragment2_to_carritoFragment)
             }
+        }
+        val botona=view.findViewById<Button>(R.id.atrasc)
+        botona.setOnClickListener{
+            findNavController().navigate(R.id.action_comentariosFragment2_to_configuracionFragment)
+        }
+
     }
 }
--- app/src/main/java/com/example/tienda_de_rosita/view/ui/fragments/ConfiguracionFragment.kt
+++ app/src/main/java/com/example/tienda_de_rosita/view/ui/fragments/ConfiguracionFragment.kt
@@ -1,61 +1,68 @@
 package com.example.tienda_de_rosita.view.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.tienda_de_rosita.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfiguracionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfiguracionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false)
+        val view = inflater.inflate(R.layout.fragment_configuracion, container, false)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConfiguracionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConfiguracionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btm= view.findViewById<BottomNavigationView>(R.id.buttonNavigationMenu)
+        btm.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.configuracionm -> findNavController().navigate(R.id.action_configuracionFragment_self)
+                R.id.homem -> findNavController().navigate(R.id.action_configuracionFragment_to_menuFragment)
+                R.id.carritom -> findNavController().navigate(R.id.action_configuracionFragment_to_carritoFragment)
             }
+        }
+
+        val btmp=view.findViewById<TextView>(R.id.texp)
+        btmp.setOnClickListener{
+            findNavController().navigate(R.id.action_configuracionFragment_to_perfilFragment)
+        }
+
+        val btmc=view.findViewById<TextView>(R.id.texc)
+        btmc.setOnClickListener{
+            findNavController().navigate(R.id.action_configuracionFragment_to_comentariosFragment2)
+        }
+
+        val btmi=view.findViewById<TextView>(R.id.texi)
+        btmi.setOnClickListener{
+            findNavController().navigate(R.id.action_configuracionFragment_to_informacionFragment)
+        }
+
+        val btmt=view.findViewById<TextView>(R.id.textt)
+        btmt.setOnClickListener{
+            findNavController().navigate(R.id.action_configuracionFragment_to_terminosFragment)
+        }
+
+        val botons=view.findViewById<Button>(R.id.buttonsalirc)
+        botons.setOnClickListener{
+            findNavController().navigate(R.id.action_configuracionFragment_to_loginActivity)
+        }
+
     }
+
 }
