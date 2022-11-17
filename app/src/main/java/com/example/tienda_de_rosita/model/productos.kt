3ff9803c7c7c94e31c2a1cbe67832c44c1511cdf
NathaN <jhonatandlopez@gmail.com>
2022-11-16 21:20:10 -0500

    Arreglo developer

--- app/src/main/java/com/example/tienda_de_rosita/model/productos.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tienda_de_rosita.model
-
-data class productos(val titulo:String,
-                    val precio:String,
-                    val imagen:String,
-                    val descripcion:String)
